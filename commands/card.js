7316297f220f29a1e483e5af969da1e03b89a8e1
TravisJEngle <TJEngle97@gmail.com>
2020-05-28 16:57:44 -0400

    Added new node dependency (request) as well as profile command
    Added profile command to the bot. Currently in testing. Also added the profile command to display user info

--- commands/card.js
+++ commands/card.js
@@ -5,52 +5,59 @@ const { profile_path } = require("../config.json");
 module.exports = {
 	name: 'card',
 	cooldown: 2,
-	description: 'testing',
+	description: 'Create a Profile Card for your Support Team. '+
+	'Some images may be from datamined assets, otherwise all units are as of '+
+	'current release in game',
 	async execute(message, args) {
 
-		const canvas = Canvas.createCanvas(1900, 800);
-		const ctx = canvas.getContext('2d');
+		if(args.length === 1 && args[0] === 'units'){
+			//Either DM the command issuer all of the units and their shorthand names for the bot, 
+			//or display the information as pages, or show a example on how characters are named for the bot
+		}else{
+			const canvas = Canvas.createCanvas(1900, 800);
+			const ctx = canvas.getContext('2d');
 
-		//const background = await Canvas.loadImage('./images/wallpaper.jpg');	
-		console.log("[CARD] DEBUG: Getting Card Image Elements");
-		// Units
-		const fire_unit = await Canvas.loadImage(profile_path+'/fire_units/'+args[0]+'.png');
-		const earth_unit = await Canvas.loadImage(profile_path+'/earth_units/'+args[1]+'.png');
-		const wind_unit = await Canvas.loadImage(profile_path+'/wind_units/'+args[2]+'.png');
-		const water_unit = await Canvas.loadImage(profile_path+'/water_units/'+args[3]+'.png');
-		const light_unit = await Canvas.loadImage(profile_path+'/light_units/'+args[4]+'.png');
-		const dark_unit = await Canvas.loadImage(profile_path+'/dark_units/'+args[5]+'.png');
+			//const background = await Canvas.loadImage('./images/wallpaper.jpg');	
+			console.log("[CARD] DEBUG: Getting Card Image Elements");
+			// Units
+			const fire_unit = await Canvas.loadImage(profile_path+'/fire_units/'+args[0]+'.png');
+			const earth_unit = await Canvas.loadImage(profile_path+'/earth_units/'+args[1]+'.png');
+			const wind_unit = await Canvas.loadImage(profile_path+'/wind_units/'+args[2]+'.png');
+			const water_unit = await Canvas.loadImage(profile_path+'/water_units/'+args[3]+'.png');
+			const light_unit = await Canvas.loadImage(profile_path+'/light_units/'+args[4]+'.png');
+			const dark_unit = await Canvas.loadImage(profile_path+'/dark_units/'+args[5]+'.png');
 
-		// Mystic Cut-in on the right side
-		const mystic = await Canvas.loadImage(profile_path+'/mystic/'+args[6]+'.png');
+			// Mystic Cut-in on the right side
+			const mystic = await Canvas.loadImage(profile_path+'/mystic/'+args[6]+'.png');
 
-		//The Basics
-		const sr_background = await Canvas.loadImage(profile_path+'/sr_background.png');
-		const card_background = await Canvas.loadImage(profile_path+'/background.png');
-		const elements = await Canvas.loadImage(profile_path+'/elements.png');
-		const title = await Canvas.loadImage(profile_path+'/title.png');
-		const border = await Canvas.loadImage(profile_path+'/borders.png');
+			//The Basics
+			const sr_background = await Canvas.loadImage(profile_path+'/sr_background.png');
+			const card_background = await Canvas.loadImage(profile_path+'/background.png');
+			const elements = await Canvas.loadImage(profile_path+'/elements.png');
+			const title = await Canvas.loadImage(profile_path+'/title.png');
+			const border = await Canvas.loadImage(profile_path+'/borders.png');
 
 
-		console.log("[CARD] DEBUG: Drawing Card Image Elements");
-		ctx.drawImage(sr_background, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(card_background, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(fire_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(water_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(wind_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(earth_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(light_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(dark_unit, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(border, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(mystic, 0, 0, canvas.width, canvas.height);
-		//Text to be rendered here
-		ctx.drawImage(elements, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(title, 0, 0, canvas.width, canvas.height);
+			console.log("[CARD] DEBUG: Drawing Card Image Elements");
+			ctx.drawImage(sr_background, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(card_background, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(mystic, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(fire_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(water_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(wind_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(earth_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(light_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(dark_unit, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(border, 0, 0, canvas.width, canvas.height);
+			//Text to be rendered here
+			ctx.drawImage(elements, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(title, 0, 0, canvas.width, canvas.height);
 
-		console.log("[CARD] DEBUG: Sending Profile Card");
-		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+			console.log("[CARD] DEBUG: Sending Profile Card");
+			const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'profile_image.png');
 
-		message.channel.send(attachment);
+			message.channel.send(attachment);
+		}
 
 	},
 };
---
+++ commands/profile.js
@@ -0,0 +1,166 @@
+const { MessageEmbed } = require('discord.js');
+const { userinfo_path } = require('../config.json');
+const request = require('request');
+const fs = require ('fs');
+
+
+module.exports = {
+	name: 'profile',
+	cooldown: 2,
+	usage: '[command name] [create] [your_name] [your_id] [*your_guild]',
+	description: 'Create a profile card for users to reference you across multiple servers',
+	execute(message, args){
+		//display profile data
+		if(args.length === 0){
+			if(!fs.existsSync(userinfo_path+'/'+message.author.id)){
+				message.reply("You don't have a profile created! Create a profile first!");
+			}else{
+				console.log('[PROFILE] DEBUG: Displaying Information');
+				var text = fs.readFileSync(userinfo_path+'/'+message.author.id+'/profile_data.txt');
+				var lines = text.split('\n');
+				var profile_name = lines[0].split(':');
+				var profile_id = lines[1].split(':');
+				var description = profile_id[1];
+				if(lines[2]){
+					var profile_guild = lines[2].split(':');
+					description+='\n'+profile_guild[1];
+				}
+				
+
+				const embed = new MessageEmbed()
+				.setTitle(profile_name[1])
+				.setColor(0xFFFFFF)
+				.setDescription(description)
+				.attachFiles(userinfo_path+'/'+message.author.id+'/profile_img.png')
+				.setImage('attachment://profile_img.png');
+				message.channel.send({embed});	
+			}
+
+		}
+		//Edit Profile
+		else if(args.length === 3 && args[0] === 'edit'){
+			if(!fs.existsSync(userinfo_path+'/'+message.author.id)){
+				message.reply("You don't have a profile created! Create a profile first!");
+				return;
+			}else{
+				var string = '';
+				var text = fs.readFileSync(userinfo_path+'/'+message.author.id+'/profile_data.txt');
+				var lines = text.split('\n');
+				//Edit Name
+				if(args[1] === 'name'){
+					var edit = lines[0].split(':');
+					edit[1] = args[2];
+					string += edit[0]+':'+edit[1]+'\n'+lines[1];
+					if(lines[2]){
+						string+='\n'+lines[2];
+					}
+					fs.writeFile(userinfo_path+'/'+message.author.id+'/profile_data.txt', string, function(error){
+						if(error) console.log(error);
+						console.log('[PROFILE] DEBUG: Profile Text edited sucessfully')
+					});
+				}
+				//Edit ID
+				else if(args[1] === 'id'){
+					var edit = lines[1].split(':');
+					edit[1] = args[2];
+					string += lines[0]+'\n'+edit[0]+':'+edit[1];
+					if(lines[2]){
+						string+='\n'+lines[2];
+					}
+					fs.writeFile(userinfo_path+'/'+message.author.id+'/profile_data.txt', string, function(error){
+						if(error) console.log(error);
+						console.log('[PROFILE] DEBUG: Profile Text edited sucessfully')
+					});
+				}
+				//Edit Image
+				else if(args[1] === 'image'){
+					if(!message.attachments.first()){
+						message.reply("Image not attached to edit!");
+						return;
+					}
+					var fileCheck = message.attachments.first().name.split('.');
+					if(fileCheck[1] === 'png'||
+						fileCheck[1] === 'jpeg'||
+						fileCheck[1] === 'jpg'){
+						console.log('[PROFILE] DEBUG: Downloading Image');
+						if(!fs.existsSync(userinfo_path+'/'+message.author.id)){
+							fs.mkdirSync(userinfo_path+'/'+message.author.id);
+						}
+						//console.log('[PROFILE] DEBUG: File URL; '+message.attachments.first().url);
+						download(message.attachments.first().url, message.author.id);
+					}else{
+						message.reply("The file you attached is not a supported type. Please use a PNG, JPG, or JPEG.");
+						return;
+					}
+
+				}
+				//Edit Guild
+				else if(args[1] === 'guild'){
+					if(lines[2]){
+						var edit = lines[2].split(':');
+						edit[1] = args[2];
+						string += lines[0]+'\n'+lines[1];
+						string+='\n'+edit[0]+':'+edit[1];
+						fs.writeFile(userinfo_path+'/'+message.author.id+'/profile_data.txt', string, function(error){
+						if(error) console.log(error);
+							console.log('[PROFILE] DEBUG: Profile Text created sucessfully')
+						});
+					}else{
+						fs.appendFile(userinfo_path+'/'+message.author.id+'/profile_data.txt', '\nguild:'+args[2]);
+					}
+				}
+				else{
+					message.reply("Invalid arguments for edit, valid arguments are [name] [id] [guild] [image]");
+					return;
+				}
+			}
+			
+		}
+		//Create Profile
+		else if(args.length >=3 && args[0] === 'create'){
+			if(fs.existsSync(userinfo_path+'/'+message.author.id)){
+				message.reply("You already have a profile created, use the edit command instead!");
+				return;
+			}
+
+
+			if(message.attachments.first()){
+				console.log('[PROFILE] DEBUG: Found Image');
+				console.log('[PROFILE] DEBUG: Filename; '+message.attachments.first().name);
+				var fileCheck = message.attachments.first().name.split('.');
+				if(fileCheck[1] === 'png'||
+					fileCheck[1] === 'jpeg'||
+					fileCheck[1] === 'jpg'){
+					console.log('[PROFILE] DEBUG: Downloading Image');
+					if(!fs.existsSync(userinfo_path+'/'+message.author.id)){
+						fs.mkdirSync(userinfo_path+'/'+message.author.id);
+					}
+					//console.log('[PROFILE] DEBUG: File URL; '+message.attachments.first().url);
+					download(message.attachments.first().url, message.author.id);
+				}else{
+					message.reply("The file you attached is not a supported type. Please use a PNG, JPG, or JPEG.");
+					return;
+				}
+			}else{
+				message.reply("Image not found to create a profile.");
+				return;
+			}
+			var content = 'name:'+args[1]+'\nid:'+args[2];
+			if(args[3]){
+				content+='\nguild:'+args[3];
+			}
+			fs.writeFile(userinfo_path+'/'+message.author.id+'/profile_data.txt', content, function(error){
+				if(error) console.log(error);
+				console.log('[PROFILE] DEBUG: Profile Text created sucessfully')
+			});
+
+		}
+		
+	},
+};
+
+function download(url, id){
+	request.get(url)
+	.on('error', console.error)
+	.pipe(fs.createWriteStream(userinfo_path+'/'+id+'/profile_img.png'));
+}
