6f3db144795e9600eaf138cf2a5dcd91b3e6da62
Jake Lovrin <jakeop4444@gmail.com>
2020-05-28 18:49:24 -0400

    Me big dumb, forgot commit

---
+++ commands/dbconnect.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+const { db_host, db_user, db_password } = require('../config.json');
+
+module.exports = {
+	name: 'dbconnect',
+	cooldown: 20,
+	description: 'Test connecting to the Database',
+	execute(message, args) {
+		var connec = mysql.createConnection({
+			host: db_host,
+			user: db_user,
+			password: db_password
+		});
+
+		connec.connect(function(error) {
+			if (error) throw error;
+			console.log("Connected to db!");
+		});
+	},
+};
