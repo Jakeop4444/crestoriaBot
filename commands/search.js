1ebf9c561467eaad40749a33bb5b0c1adc0f840e
TravisJEngle <TJEngle97@gmail.com>
2020-05-28 23:38:22 -0400

    Update search.js
    Fixed SQL displaying results

--- commands/search.js
+++ commands/search.js
@@ -1,12 +1,13 @@
 var mysql = require('mysql');
 const { db_host, db_user, db_password, db_name, db_table } = require('../config.json');
-var q_name = '*', q_element = '*', q_rarity = '*', q_type = '*';
+
 
 module.exports = {
 	name: 'search',
 	cooldown: 5,
 	description: 'Find and display a Unit!',
 	execute(message, args) {
+		var q_name = '*', q_element = '*', q_rarity = '*', q_type = '*';
 		var connec = mysql.createConnection({
 			host: db_host,
 			user: db_user,
@@ -34,16 +35,40 @@ module.exports = {
 		}
 
 		
-		var sql = "SELECT Name, Element, Rarity, Type FROM characters WHERE Name = 'Sorey'";
+		var sql = "SELECT Name, Title, Element, Rarity, Type FROM characters WHERE";
+		if(q_name != '*'){
+			sql += " Name = '"+q_name+"'";
+		}
+		if(q_element != '*'){
+			if(q_name != '*'){
+				sql += " AND";
+			}
+			sql += " Element = '"+q_element+"'";	
+		}
+		if(q_rarity != '*'){
+			if(q_name != '*' || q_element != '*'){
+				sql += " AND";	
+			}
+			sql += " Rarity = '"+q_rarity+"'";
+		}
+		if(q_type != '*'){
+			if(q_name != '*' || q_element != '*' || q_rarity != '*'){
+				sql += " AND";
+			}
+			sql += " Rarity = '"+q_type+"'";
+		}
 		connec.query(sql, function (error, result, fields) {
 			if (error) console.log(error);
-			console.log("Result:" + result);
-			console.log("Fields:" + fields);
-			console.log(result[0].Name+" "+result[0].Element+" "+result[0].Rarity+" "+result[0].Type);
+			//console.log("Result:" + result);
+			//console.log("Fields:" + fields);
+			console.log(sql);
+			console.log(result[0].Name+" "+result[0].Title+" "+result[0].Element+" "+result[0].Rarity+" "+result[0].Type);
 		});
-		/*var sql = "SELECT * FROM characters WHERE Name = ? AND Element = ? AND Rarity = ? AND Type = ?";
-		connec.query(sql, [q_name, q_element, q_rarity, q_type], function (error, result) {
+		/*var sql = "SELECT Name, Title, Element, Rarity, Type FROM characters WHERE Name = "+q_name;//+"' AND Element = '"+q_element+"' AND Rarity = '"+q_rarity+"' AND Type = '"+q_type+"'";
+		//var sql = "SELECT * FROM characters WHERE Name = ? AND Element = ? AND Rarity = ? AND Type = ?";
+		connec.query(sql, function (error, result, fields) {
 			if (error) console.log(error);
+			console.log(sql);
 			console.log("Result:" + result);
 		});*/
 		
