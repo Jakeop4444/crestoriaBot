3d220f8ef36ad6d2a3212a9dfaa5f8c4e3796b83
Jake Lovrin <jakeop4444@gmail.com>
2020-06-01 20:38:48 -0400

    help now only sends directly to dms

--- commands/help.js
+++ commands/help.js
@@ -41,6 +41,15 @@ module.exports = {
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
-		message.channel.send(data, { split: true });
+		return message.author.send(data, { split: true})
+			.then(() => {
+				if (message.channel.type === 'dm') return;
+				message.reply('I\'ve sent you a DM with the specifics!');
+			})
+			.catch(error => {
+				console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+				message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+			});
+		//message.channel.send(data, { split: true });
 	},
 };
