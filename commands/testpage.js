1558110f9e85923d6a86586d5d60fa3f0dd8e9d6
Jake Lovrin <jakeop4444@gmail.com>
2020-06-04 21:26:45 -0400

    added first pagination test

---
+++ commands/testpage.js
@@ -0,0 +1,23 @@
+const Pagination = require('discord-paginationembed');
+
+module.exports = {
+	name: 'testpage',
+	cooldown: 2,
+	description: 'Testing pagination using Discord PaginationEmbed utility.',
+	execute(message, args) {
+		const FieldsEmbed = new Pagination.FieldsEmbed()
+			.setArray([{name: 'Lloyd'}, {name: 'Edna'}])
+			.setAuthorizedUsers([message.author.id])
+			.setChannel(message.channel)
+			.setElementsPerPage(1)
+			.setPage(1)
+			.setPageIndicator(true)
+			.setDeleteOnTimeout(true);
+
+		FieldsEmbed.embed
+			.setColor(0xFF00AE)
+			.setDescription('Test Description');
+
+		FieldsEmbed.build();
+	},
+};
