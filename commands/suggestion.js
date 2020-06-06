41f9bf80fc26b656839b0c1612a617d8b069a181
HellFyre <TJEngle97@gmail.com>
2020-06-06 00:56:48 -0400

    Addition of suggestion command
    Allowing users to submit ideas to vision orb for future consideration

---
+++ commands/suggestion.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+module.exports = {
+	name: 'suggestion',
+	cooldown: 2,
+	description: "Submit a suggestion to VisionOrb's Dev Team",
+	execute(message, args) {
+		var string = "";
+
+		for(i = 0; i<args.length; i++){
+			string+=(args[i]+" ");
+		}
+		string+="\n\n";
+		fs.appendFile("./suggestions/suggestions.txt", string, function (err) {
+			if (err) throw err;
+			message.reply("Your suggestion has been submitted!");
+		});
+	},
+};
